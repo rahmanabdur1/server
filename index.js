b1b3fcb6d67da7f2fb07f0fd39262d37c5f18049
rahmanabdur1 <rahmanabdurr65@gmail.com>
2024-04-05 19:50:08 -0700

    code updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -23,55 +23,47 @@ const client = new MongoClient(uri, {
 
 
 async function run() {
-    try {
-      await client.connect();
-      console.log('Connected to MongoDB');
-  
-      const db = client.db("loginusers");
-      const loginusers = db.collection("loginusers");
-
-      app.get('/users', async (req, res) => {
-        const query = {};
-        const users = await loginusers.find(query).toArray();
-        console.log(users,'us')
-        res.send(users)
-      })
-
-  // Update the server-side code to include the delete user route
-app.delete('/users/:id', async (req, res) => {
-  const userId = req.params.id;
   try {
-      const result = await loginusers.deleteOne({ _id: userId });
-      if (result.deletedCount === 1) {
-          res.status(200).json({ message: 'User deleted successfully' });
-      } else {
-          res.status(404).json({ message: 'User not found' });
-      }
-  } catch (error) {
-      console.error('Error deleting user:', error);
-      res.status(500).json({ message: 'Internal server error' });
-  }
-});
+    await client.connect();
+    console.log('Connected to MongoDB');
 
-      app.post('/users', async (req, res) => {
-        const contact = req.body;
-      const result = await loginusers.insertOne(contact);
-      console.log(result,'r')
+    const db = client.db("loginusers");
+    const loginusers = db.collection("loginusers");
+
+    app.get('/users', async (req, res) => {
+      const query = {};
+      const users = await loginusers.find(query).toArray();
+      res.send(users)
+    })
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id,'d')
+      const filter = { _id: new ObjectId(id) }
+      const result = await loginusers.deleteOne(filter);
+      console.log(result, 'r')
       res.send(result)
-      });
-  
-    } catch (error) {
-      console.error('Error connecting to MongoDB:', error);
-      process.exit(1);
-    }
+    });
+
+
+    app.post('/users', async (req, res) => {
+      const contact = req.body;
+      const result = await loginusers.insertOne(contact);
+      res.send(result)
+    });
+
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
   }
-  
-  run().then(() => {
-    app.get('/', (req, res) => {
-      res.send('Doctors portal server is running');
-    });
-  
-    app.listen(port, () => {
-      console.log(`Doctors server listening on port ${port}`);
-    });
-  }).catch(console.error);
+}
+
+run().then(() => {
+  app.get('/', (req, res) => {
+    res.send('Doctors portal server is running');
+  });
+
+  app.listen(port, () => {
+    console.log(`Doctors server listening on port ${port}`);
+  });
+}).catch(console.error);
